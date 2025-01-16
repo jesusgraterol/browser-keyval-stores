85536e1a803cdcb54adedc6ebbfac6b920c8a550
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2025-01-16 14:43:48 -0400

    Include remaining JSON related errors
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/shared/errors.ts
+++ src/shared/errors.ts
@@ -3,10 +3,15 @@
 /* ************************************************************************************************
  *                                         IMPLEMENTATION                                         *
  ************************************************************************************************ */
-type IError = 'INVALID_JSON_DATA';
+type IError = 'INVALID_JSON_DATA' | 'FAILED_TO_STRINGIFY_JSON' | 'FAILED_TO_PARSE_JSON';
 const ERRORS: { [key in IError]: IError } = {
   INVALID_JSON_DATA: 'INVALID_JSON_DATA',
+  FAILED_TO_STRINGIFY_JSON: 'FAILED_TO_STRINGIFY_JSON',
+  FAILED_TO_PARSE_JSON: 'FAILED_TO_PARSE_JSON',
 };
+
+
+
 
 
 /* ************************************************************************************************
