8019738064d752e7d4a73b18d7570b3db45f4dee
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2025-01-16 13:16:05 -0400

    Implement unit tests for the TempMemoryStore Class
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/stores/temp-memory/index.test-unit.ts
+++ src/stores/temp-memory/index.test-unit.ts
@@ -1,11 +1,33 @@
-import { describe, beforeAll, afterAll, beforeEach, afterEach, test, expect, vi } from 'vitest';
+import { describe, test, expect } from 'vitest';
+import { TempMemoryStore } from './index.js';
 
 /* ************************************************************************************************
  *                                             TESTS                                              *
  ************************************************************************************************ */
 
 describe('TempMemoryStore', () => {
-  test.skip('can calculate 2 plus 2', () => {
-    expect(2 + 2).toBe(4);
+  test('can instantiate a store and manage the root of it', () => {
+    const store = new TempMemoryStore('unit_test');
+    expect(store.id).toBe('unit_test');
+    expect(store.isCompatible).toBe(true);
+    expect(store.get()).toBeUndefined();
+    store.set(undefined, { foo: 'bar' });
+    expect(store.get()).toStrictEqual({ foo: 'bar' });
+    store.set(undefined, { foo: 'bar2' });
+    expect(store.get()).toStrictEqual({ foo: 'bar2' });
+    store.del();
+    expect(store.get()).toBeUndefined();
+  });
+
+  test('can instantiate a store and manage its data based on record ids', () => {
+    const store = new TempMemoryStore('unit_test_2');
+    const id = '16ceb975-4989-4c86-8b63-99294e86a13d';
+    expect(store.get(id)).toBeUndefined();
+    store.set(id, { foo: 'bar' });
+    expect(store.get(id)).toStrictEqual({ foo: 'bar' });
+    store.set(id, { foo: 'bar2' });
+    expect(store.get(id)).toStrictEqual({ foo: 'bar2' });
+    store.del(id);
+    expect(store.get(id)).toBeUndefined();
   });
 });
