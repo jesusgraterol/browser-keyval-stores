87385ff5efe79f7120e3f28ea76cb03ca29b4935
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2025-01-17 13:38:28 -0400

    Remove mock code
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/index.ts
+++ src/index.ts
@@ -6,29 +6,6 @@ import {
   SessionStorageStore,
 } from './stores/index.js';
 
-type IApplicationFormData = {
-  fullName: string,
-  passportNum: string,
-};
-
-const store = new SessionStorageStore<IApplicationFormData>('applicationForm');
-
-store.get();
-// undefined
-
-store.set(undefined, { fullName: 'Jesus Graterol', passportNum: 'P4366918' });
-store.get();
-// { fullName: 'Jesus Graterol', passportNum: 'P4366918' }
-
-store.set(undefined, { fullName: 'Jesus Graterol', passportNum: 'LA080402' });
-store.get();
-// { fullName: 'Jesus Graterol', passportNum: 'LA080402' }
-
-store.del();
-store.get();
-// undefined
-
-
 /* ************************************************************************************************
  *                                         MODULE EXPORTS                                         *
  ************************************************************************************************ */
